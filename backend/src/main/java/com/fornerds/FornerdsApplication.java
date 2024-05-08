c86a299caab7ea031551b8617a63ba631d7dff1b
handevmin <handevmin@gmail.com>
2024-05-08 18:56:24 +0900

    Initial setup of backend folder structure

---
+++ backend/src/main/java/com/fornerds/FornerdsApplication.java
@@ -0,0 +1,12 @@
+package com.fornerds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FornerdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FornerdsApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/fornerds/FornerdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fornerds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FornerdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
