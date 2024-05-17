b947473b0f2e7ee350fadc063d563873ed0e3748
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2024-05-17 17:51:56 +0900

    feat: color css

--- frontend/src/components/AtomComponent/Tag/Tag.tsx
+++ frontend/src/components/AtomComponent/Tag/Tag.tsx
@@ -3,12 +3,18 @@ import styles from './Tag.module.css'
 
 interface TagProps {
   children: ReactNode
-  variant?: 'ongoing' | 'finished' | 'disabled' | 'info' | 'default'
+  variant?: 'active' | 'default' | 'disabled'
   className?: string
+  rounded?: boolean
 }
 
-export function Tag({ children, variant = 'info', className = '' }: TagProps) {
-  const tagClass = `${styles.tag} ${styles[variant]} ${className}`
+export function Tag({
+  children,
+  variant = 'default',
+  className = '',
+  rounded
+}: TagProps) {
+  const tagClass = `${styles.tag} ${styles[variant]} ${rounded ? styles.rounded : ''} ${className}`
 
   return <span className={tagClass}>{children}</span>
 }
