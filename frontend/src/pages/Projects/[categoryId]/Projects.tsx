671e8b2ff97e928ad28ad51ceb6466402aa07361
NOHA <gammagx@gmail.com>
2024-05-19 18:00:04 +0900

    feat: footer css / header, footer 확인용 간단한 page layout

--- frontend/src/pages/Projects/[categoryId]/Projects.tsx
+++ frontend/src/pages/Projects/[categoryId]/Projects.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styles from './Projects.module.css'
-import { Header } from '../../../components/ModuleComponent'
+import { Footer, Header } from '../../../components/ModuleComponent'
 
 export function Projects() {
   return (
-    <>
+    <div className={styles.pageLayout}>
       <Header />
-    </>
+      <div className={styles.content}></div>
+      <Footer />
+    </div>
   )
 }
