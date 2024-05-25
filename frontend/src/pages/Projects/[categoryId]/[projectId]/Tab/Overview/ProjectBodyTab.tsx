6b414d2f881e5863a992e31488c098d6191ff16d
NOHA <gammagx@gmail.com>
2024-05-25 22:03:46 +0900

    design: overviewTab 영역에 display flex & gap 넣기 (#11)
    what : ProjectTab.module.css 파일 다시 생성 / css 추가
    why  : tab 연결하면서 css 누락 된 부분 확인하여 다시 추가
    
    Related to: #11

--- frontend/src/pages/Projects/[categoryId]/[projectId]/Tab/Overview/ProjectBodyTab.tsx
+++ frontend/src/pages/Projects/[categoryId]/[projectId]/Tab/Overview/ProjectBodyTab.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from './ProjectBodyTab.module.css'
 import { TextField } from './TextField'
 import { ProjectDetailCards } from './ProjectDetailCards'
 
 export function ProjectBodyTab() {
   return (
-    <div>
+    <div className={styles.contents}>
       <ProjectDetailCards />
       <TextField />
     </div>
