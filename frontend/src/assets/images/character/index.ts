ee3c65f6c524ddc0f113fad9b886f85a0126934b
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2024-05-27 20:33:35 +0900

    feat: QuestDetail Solution Table

---
+++ frontend/src/assets/images/character/index.ts
@@ -0,0 +1,25 @@
+import charactor01 from './character01.webp'
+import charactor02 from './character02.webp'
+import charactor03 from './character03.webp'
+import charactor04 from './character04.webp'
+import charactor05 from './character05.webp'
+import charactor06 from './character06.webp'
+import charactor07 from './character07.webp'
+import charactor08 from './character08.webp'
+import charactor09 from './character09.webp'
+import charactor10 from './character10.webp'
+
+const characters = [
+  charactor01,
+  charactor02,
+  charactor03,
+  charactor04,
+  charactor05,
+  charactor06,
+  charactor07,
+  charactor08,
+  charactor09,
+  charactor10
+]
+
+export default characters
