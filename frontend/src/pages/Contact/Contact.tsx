2999d74b9c00f8f3d119d0715f6985db1e35f4a0
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2024-05-30 17:53:20 +0900

    feat: contact page

--- frontend/src/pages/Contact/Contact.tsx
+++ frontend/src/pages/Contact/Contact.tsx
@@ -1,6 +1,82 @@
 import React from 'react'
 import styles from './Contact.module.css'
+import { Footer, Header } from '../../components/ModuleComponent'
+import backgroundTop from '../../assets/images/background/background03.webp'
+import { Button, Input } from '../../components/AtomComponent'
 
 export function Contact() {
-  return <></>
+  return (
+    <>
+      <div className={styles.background}>
+        <div
+          className={styles.backgroundImage}
+          style={{ backgroundImage: `url(${backgroundTop})` }}
+        ></div>
+        <div className={styles.content}>
+          <Header />
+          <main className={styles.main}>
+            <h2 className="font-pixellari-header-3 text-color-lighten">
+              Always welcome to chat
+            </h2>
+            <form className={styles.form}>
+              <section className={styles.inputs}>
+                <div className={styles.inputWrap}>
+                  <label
+                    htmlFor="name"
+                    className="font-roboto-cta-small text-color-lighten"
+                  >
+                    Name
+                  </label>
+                  <Input
+                    className={styles.input}
+                    type="text"
+                    name="name"
+                    id="name"
+                    placeholder="Add to the name"
+                  ></Input>
+                </div>
+                <div className={styles.inputWrap}>
+                  <label
+                    htmlFor="email"
+                    className="font-roboto-cta-small text-color-lighten"
+                  >
+                    Email
+                  </label>
+                  <Input
+                    className={styles.input}
+                    type="email"
+                    name="email"
+                    id="email"
+                    placeholder="Add to the email"
+                  ></Input>
+                </div>
+                <div className={styles.inputWrap}>
+                  <label
+                    htmlFor="subject"
+                    className="font-roboto-cta-small text-color-lighten"
+                  >
+                    Subject
+                  </label>
+                  <textarea
+                    className={`${styles.input} ${styles.textarea}`}
+                    name="subject"
+                    id="subject"
+                    placeholder="Add to the discussion"
+                  ></textarea>
+                </div>
+              </section>
+              <Button
+                className={`${styles.button} font-roboto-cta-small text-color-lighten`}
+                type="submit"
+                variant="active"
+              >
+                Submit
+              </Button>
+            </form>
+          </main>
+        </div>
+        <Footer />
+      </div>
+    </>
+  )
 }
