890fafa8f0d2b2b332dc4391f4c2491142f242f1
PUNGY <71264780+KANGPUNGYUN@users.noreply.github.com>
2024-06-01 15:50:15 +0900

    feat: mypage mysolutions

--- frontend/src/components/ModuleComponent/Table/Table.tsx
+++ frontend/src/components/ModuleComponent/Table/Table.tsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './Table.module.css'
-import { SolutionProps } from './TableProps'
+import { SolutionProps, MySolutionProps, SolutionsTapProps } from './TableProps'
 import characters from '../../../assets/images/character'
+import { Bookmark } from '../Bookmark'
 
 const formatDateToKoreanFormat = (isoString: string): string => {
   const date = new Date(isoString)
@@ -63,6 +64,91 @@ export function SolutionTable(props: SolutionProps) {
       </div>
       <div
         className={`${styles.solutionTableItem} font-roboto-body-1 text-color-default`}
+      >
+        {formatDateToKoreanFormat(props.createdAt)}
+      </div>
+    </li>
+  )
+}
+
+export function MySolutionTable(props: MySolutionProps) {
+  return (
+    <li className={styles.mySolutionList}>
+      <div
+        className={`${styles.mySolutionCenterItem} font-roboto-body-1 text-color-lighten`}
+      >
+        {props.likeCount}
+      </div>
+      <div
+        className={`${styles.mySolutionItem} font-roboto-body-1 text-color-lighten`}
+      >
+        {props.title}
+      </div>
+      <div
+        className={`${styles.mySolutionCenterItem} font-roboto-body-1 text-color-lighten`}
+      >
+        <img
+          src={characters[props.user_image]}
+          alt={`Character ${props.user_image}`}
+          width="36"
+          height="36"
+        />
+      </div>
+      <div
+        className={`${styles.mySolutionItem} font-roboto-body-1 text-color-lighten`}
+      >
+        {props.memoryUsage} KB
+      </div>
+      <div
+        className={`${styles.mySolutionItem} font-roboto-body-1 text-color-default`}
+      >
+        {formatDateToKoreanFormat(props.createdAt)}
+      </div>
+    </li>
+  )
+}
+
+export function SolutionsTabTable(props: SolutionsTapProps) {
+  const [isBookmarked, setIsBookmarked] = useState(props.isBookmarked || false)
+
+  const handleBookmarkClick = () => {
+    setIsBookmarked(!isBookmarked)
+  }
+
+  return (
+    <li className={styles.mySolutionList}>
+      <div
+        className={`${styles.mySolutionCenterItem} font-roboto-body-1 text-color-lighten`}
+      >
+        <Bookmark
+          width="24"
+          height="24"
+          isBookmarked={props.isBookmarked}
+          onBookmarkClick={handleBookmarkClick}
+        />
+      </div>
+      <div
+        className={`${styles.mySolutionItem} font-roboto-body-1 text-color-lighten`}
+      >
+        {props.title}
+      </div>
+      <div
+        className={`${styles.mySolutionCenterItem} font-roboto-body-1 text-color-lighten`}
+      >
+        <img
+          src={characters[props.user_image]}
+          alt={`Character ${props.user_image}`}
+          width="36"
+          height="36"
+        />
+      </div>
+      <div
+        className={`${styles.mySolutionItem} font-roboto-body-1 text-color-lighten`}
+      >
+        {props.memoryUsage} KB
+      </div>
+      <div
+        className={`${styles.mySolutionItem} font-roboto-body-1 text-color-default`}
       >
         {formatDateToKoreanFormat(props.createdAt)}
       </div>
