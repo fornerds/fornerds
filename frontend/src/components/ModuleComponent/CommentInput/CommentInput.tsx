082ef14d85b310beddbae0c52082db4a8eb2d1cf
짱짱맨 <60203153+kksjjm@users.noreply.github.com>
2024-06-04 20:00:10 +0900

    feat/ 댓글, 대댓글 추가시 등록, 더 불러오기 기능 추가

--- frontend/src/components/ModuleComponent/CommentInput/CommentInput.tsx
+++ frontend/src/components/ModuleComponent/CommentInput/CommentInput.tsx
@@ -6,25 +6,35 @@ import { Input } from '../../AtomComponent'
 interface CommentInputProps {
   isReply?: boolean
   buttonText: string | null
-  onSubmit: () => void
+  onSubmit: (content: string) => void
 }
 
-export function CommentInput({ 
+export function CommentInput({
   isReply = false,
   buttonText = 'Submit',
   onSubmit }: CommentInputProps) {
 
+  const [content, setContent] = React.useState('');
   const textareaClass = isReply ? styles.replyTextarea : styles.textarea
+
+  const handleSubmit = () => {
+    onSubmit(content);
+    setContent('');
+  };
 
   return (
     <div className={styles.commentInput}>
       <div className={styles.character}></div>
       <div className={styles.inputBox}>
-        <Input className={textareaClass} placeholder='Add to the discussion'>
+        <Input
+          className={textareaClass}
+          placeholder='Add to the discussion'
+          value={content}
+          onChange={(e) => setContent(e.target.value)}>
         </Input>
         <Button
           className={styles.submitButton}
-          onClick={onSubmit}
+          onClick={handleSubmit}
         >
           {buttonText}
         </Button>
